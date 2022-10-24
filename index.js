ae9db913e2504ea8f29296d606530beeca46089b
ahsan <ahsanejaz99@gmail.com>
2022-10-24 12:06:01 +0500

    with

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.get('/routes', (req, res)=>{
 
 
     res.json({
-        "message": "What is your name"
+        "message": "What is your name",
+        "image": "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80"
     })
 
 
